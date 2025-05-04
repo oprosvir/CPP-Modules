6f0e3e351522437c0e70c2ecd9018ce732ce80b4
oprosvir <ol7897@ya.ru>
2025-05-04 21:13:55 +0200

    cpp00/ex00 finished

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # CPP Modules
 
 This repository contains a collection of C++ exercises, organized into modules. Each module focuses on different aspects of C++ programming.
+
+## 📚 Module Overview
+
+- [cpp00](./cpp00)  
+  *Introduction to C++ syntax and structure: namespaces, classes, member functions, stdio streams, initialization lists, static, const, and other basic concepts.*
---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/03 23:40:37 by oprosvir          #+#    #+#             */
+/*   Updated: 2025/05/04 20:47:17 by oprosvir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)std::toupper(argv[i][j]);
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return 0;
+}
