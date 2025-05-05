0b99bcfb8e597a5a500711d9214801bcf21385ea
oprosvir <ol7897@ya.ru>
2025-05-05 23:15:18 +0200

    cpp00/ex00 rewrite w/string type

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 23:40:37 by oprosvir          #+#    #+#             */
-/*   Updated: 2025/05/04 20:47:17 by oprosvir         ###   ########.fr       */
+/*   Updated: 2025/05/05 22:17:45 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 int	main(int argc, char *argv[])
 {
-	if (argc > 1)
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
 	{
-		for (int i = 1; i < argc; i++)
+		for (int i = 1; i < argc; ++i)
 		{
-			for (int j = 0; argv[i][j]; j++)
-				std::cout << (char)std::toupper(argv[i][j]);
+			std::string arg = argv[i];
+			for (size_t j = 0; j < arg.size(); ++j)
+				arg[j] = std::toupper(arg[j]);
+			std::cout << arg;
 		}
 		std::cout << std::endl;
 	}
-	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	return 0;
 }
