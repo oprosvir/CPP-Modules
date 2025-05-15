0c31d2c51333c249ebc406e0bffe0e46a2e471a8
oprosvir <ol7897@ya.ru>
2025-05-15 16:21:52 +0200

    cpp02/ex00 improved, assigment operator must not be used in copy consructor

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 21:26:11 by oprosvir          #+#    #+#             */
-/*   Updated: 2025/05/14 00:23:24 by oprosvir         ###   ########.fr       */
+/*   Updated: 2025/05/15 15:53:48 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ Fixed::~Fixed() {
 // Copy constructor
 Fixed::Fixed(const Fixed& other) {
     std::cout << "Copy constructor called" << std::endl;
-    *this = other;
+    this->_rawBits = other.getRawBits();
 }
 
-// Copy assignment operator
+// Copy assignment operator overload
 Fixed& Fixed::operator=(const Fixed& other) {
     std::cout << "Copy assignment operator called" << std::endl;
     if (this != &other)
