99b6497e625fd4694acc0a43aae318ad5aa3a38d
oprosvir <ol7897@ya.ru>
2025-05-16 18:23:58 +0200

    cpp02/ex00 unnessesary this-> del

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 21:26:11 by oprosvir          #+#    #+#             */
-/*   Updated: 2025/05/15 15:53:48 by oprosvir         ###   ########.fr       */
+/*   Updated: 2025/05/15 16:40:17 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ Fixed::~Fixed() {
 // Copy constructor
 Fixed::Fixed(const Fixed& other) {
     std::cout << "Copy constructor called" << std::endl;
-    this->_rawBits = other.getRawBits();
+    _rawBits = other.getRawBits();
 }
 
 // Copy assignment operator overload
 Fixed& Fixed::operator=(const Fixed& other) {
     std::cout << "Copy assignment operator called" << std::endl;
     if (this != &other)
-        this->_rawBits = other.getRawBits();
+        _rawBits = other.getRawBits();
     return *this;
 }
 
