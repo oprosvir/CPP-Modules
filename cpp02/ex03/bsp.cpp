03200a48b2c2d0f96a6f39921c9742f05b6fcc6a
oprosvir <ol7897@ya.ru>
2025-05-19 00:56:13 +0200

    cpp02/ex03 small improvements

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,13 +6,13 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 18:39:35 by oprosvir          #+#    #+#             */
-/*   Updated: 2025/05/19 00:44:19 by oprosvir         ###   ########.fr       */
+/*   Updated: 2025/05/19 00:54:15 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-Fixed sign(Point const& p1, Point const& p2, Point const& p3) {
+static Fixed sign(Point const& p1, Point const& p2, Point const& p3) {
     return (p1.getX() - p3.getX()) * (p2.getY() - p3.getY()) -
            (p2.getX() - p3.getX()) * (p1.getY() - p3.getY());
 }
@@ -22,11 +22,11 @@ bool bsp(Point const a, Point const b, Point const c, Point const point) {
     Fixed d2 = sign(point, b, c);
     Fixed d3 = sign(point, c, a);
 
-    if (d1 == Fixed(0) || d2 == Fixed(0) || d3 == Fixed(0))
+    if (d1 == 0 || d2 == 0 || d3 == 0)
         return false;   // point lies on edge or vertex
 
-    bool has_neg = (d1 < Fixed(0)) || (d2 < Fixed(0)) || (d3 < Fixed(0));
-    bool has_pos = (d1 > Fixed(0)) || (d2 > Fixed(0)) || (d3 > Fixed(0));
+    bool has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+    bool has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
 
     return !(has_neg && has_pos);
 }
