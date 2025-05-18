c9178fc6f3cbd715f0336ac256b9809e579f19aa
oprosvir <ol7897@ya.ru>
2025-05-18 23:06:05 +0200

    cpp02/ex01 main tested

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 16:25:09 by oprosvir          #+#    #+#             */
-/*   Updated: 2025/05/16 17:21:47 by oprosvir         ###   ########.fr       */
+/*   Updated: 2025/05/18 21:34:44 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,26 @@
 
 int main(void) {
     Fixed a;
-	a.setRawBits(10995);
 	Fixed const b(10);
-	// Fixed const c(42.42f);
-	// Fixed const d(b);
+	Fixed const c(42.42f);
+	Fixed const d(b);
 
-	// a = Fixed(1234.4321f);
-	std::cout << "a is " << b.getRawBits() << " raw bits"<< std::endl;
+	a = Fixed(1234.4321f);
+
+	std::cout << "a is " << a.getRawBits() << " raw bits"<< std::endl;
+	std::cout << "b is " << b.getRawBits() << " raw bits"<< std::endl;
+	std::cout << "c is " << c.getRawBits() << " raw bits"<< std::endl;
+	std::cout << "d is " << d.getRawBits() << " raw bits"<< std::endl;
+	
 	std::cout << "a is " << a << std::endl;
-	// std::cout << "b is " << b << std::endl;
-	// std::cout << "c is " << c << std::endl;
-	// std::cout << "d is " << d << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	// std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	// std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	// std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
     
     return 0;
 }
