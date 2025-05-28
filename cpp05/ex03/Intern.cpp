8d546beb923221df48d3eb71136ce07885691be8
oprosvir <ol7897@ya.ru>
2025-05-28 12:07:43 +0200

    cpp05/ex03 fin

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 22:42:16 by oprosvir          #+#    #+#             */
-/*   Updated: 2025/05/27 22:49:04 by oprosvir         ###   ########.fr       */
+/*   Updated: 2025/05/28 11:25:19 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,42 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include <iostream>
 
 Intern::Intern() {}
 Intern::Intern(const Intern&) {}
 Intern& Intern::operator=(const Intern&) { return *this; }
 Intern::~Intern() {}
+
+static AForm* createShrubbery(const std::string& target) {
+    return new ShrubberyCreationForm(target);
+}
+
+static AForm* createRobotomy(const std::string& target) {
+    return new RobotomyRequestForm(target);
+}
+
+static AForm* createPresidential(const std::string& target) {
+    return new PresidentialPardonForm(target);
+}
+
+AForm* Intern::makeForm(const std::string& formName, const std::string& target) {
+    const std::string formNames[] = {
+        "shrubbery creation",
+        "robotomy request",
+        "presidential pardon"
+    };
+    AForm* (*constructors[])(const std::string&) = {
+        &createShrubbery,
+        &createRobotomy,
+        &createPresidential
+    };
+    for (int i = 0; i < 3; ++i) {
+        if (formName == formNames[i]) {
+            std::cout << "Intern creates " << formName << std::endl;
+            return constructors[i](target);
+        }
+    }
+    std::cout << "Intern: unknown form \"" << formName << "\". Cannot create.\n";
+    return NULL;
+}
--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 22:41:14 by oprosvir          #+#    #+#             */
-/*   Updated: 2025/05/27 22:56:33 by oprosvir         ###   ########.fr       */
+/*   Updated: 2025/05/27 23:19:00 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,26 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include <ctime>
 
 int main(void) {
+    std::srand(std::time(0));
     std::cout << "\n=== Intern — valid form creation ===\n";
 
     Intern someIntern;
     AForm* form = someIntern.makeForm("robotomy request", "Bender");
-
     if (form) {
         std::cout << *form << std::endl;
-
         Bureaucrat admin("Admin", 1);
+        std::cout << admin << std::endl;
         admin.signForm(*form);
         admin.executeForm(*form);
-
         delete form;
     }
 
     std::cout << "\n=== Intern — unknown form ===\n";
 
     AForm* invalidForm = someIntern.makeForm("pardon me please", "Nobody");
-
     if (!invalidForm) {
         std::cout << "Form creation failed as expected.\n";
     } else {
