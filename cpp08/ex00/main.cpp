d82e07a0ce6ecb83a52df67bd250e7d67d0f222f
oprosvir <ol7897@ya.ru>
2025-06-08 12:06:24 +0200

    cpp08/ex00 sets added, erase tests

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 21:52:48 by oprosvir          #+#    #+#             */
-/*   Updated: 2025/06/07 23:06:18 by oprosvir         ###   ########.fr       */
+/*   Updated: 2025/06/08 12:05:23 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 #include <vector>
 #include <list>
 #include <deque>
+#include <set>
 
 int main(void) {
-    std::cout << "--- std::vector (mutable) ---" << std::endl;
+    std::cout << "--- std::vector (mutable, duplicates) ---" << std::endl;
     std::vector<int> v;
     v.push_back(10);
     v.push_back(20);
     v.push_back(30);
+    v.push_back(20);
     std::cout << "Vector before: ";
     printContainer(v);
     
@@ -29,12 +31,15 @@ int main(void) {
         std::cout << "Found in vector: " << *it << std::endl;
         *it = 42;
         std::cout << "Modified to: " << *it << std::endl;
+        std::cout << "Vector after modification: ";
+        printContainer(v);
+        std::cout << "Erasing modified element ..." << std::endl;
+        v.erase(it);
+        std::cout << "Vector after erase: ";
+        printContainer(v);
     } catch (const std::exception& e) {
         std::cout << "Vector: " << e.what() << std::endl;
     }
-
-    std::cout << "Vector after: ";
-    printContainer(v);
 
     std::cout << "\n--- std::list (const) ---" << std::endl;
     std::list<int> temp;
@@ -75,6 +80,44 @@ int main(void) {
 
     std::cout << "Deque after: ";
     printContainer(d);
+
+    std::cout << "\n--- std::set (ordered) ---" << std::endl;
+    std::set<int> s;
+    s.insert(42);
+    s.insert(800);
+    s.insert(-5);
+
+    std::cout << "Set: ";
+    printContainer(s);   // sorted
+
+    try {
+        std::set<int>::iterator it = easyfind(s, 100);
+        std::cout << "Found in set: " << *it << std::endl;
+        // *it = 999; // set is const
+    } catch (const std::exception& e) {
+        std::cout << "Set: " << e.what() << std::endl;
+    }
+
+    std::cout << "\n--- std::multiset (duplicates allowed) ---" << std::endl;
+    std::multiset<int> ms;
+    ms.insert(10000);
+    ms.insert(-42);
+    ms.insert(0);
+    ms.insert(-42);
+
+    std::cout << "Multiset before: ";
+    printContainer(ms);
+
+    try {
+        std::multiset<int>::iterator it = easyfind(ms, -42);
+        std::cout << "Found: " << *it << ", removing..." << std::endl;
+        ms.erase(it);
+    } catch (const std::exception& e) {
+        std::cout << "Multiset: " << e.what() << std::endl;
+    }
+
+    std::cout << "Multiset after: ";
+    printContainer(ms);
     
     return 0;
 }
