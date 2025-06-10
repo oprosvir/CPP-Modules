238ded93c25c62217f7804057f759e41c3e3c4ee
oprosvir <ol7897@ya.ru>
2025-06-10 20:33:33 +0200

    cpp08/ex02 add more tests

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,13 +6,16 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 10:31:00 by oprosvir          #+#    #+#             */
-/*   Updated: 2025/06/10 19:54:28 by oprosvir         ###   ########.fr       */
+/*   Updated: 2025/06/10 20:29:21 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include "../ex00/easyfind.hpp"
+#include <algorithm>
 #include <iostream>
 #include <list>
+#include <numeric>
 
 int main(void) {
 	std::cout << "--- MutantStack basic test ---" << std::endl;
@@ -71,7 +74,65 @@ int main(void) {
 		std::cout << *lit << std::endl;
 		++lit;
 	}
-	std::stack<int, std::list<int> > copy(lst);
+	std::stack<int, std::list<int> > clst(lst);
 
+    std::cout << "\n--- Const MutantStack iteration ---" << std::endl;
+    const MutantStack<int> constStack = mstack;
+    printContainer(constStack);
+
+    std::cout << "\n--- Copy and assignment ---" << std::endl;
+    MutantStack<int> copy(mstack);
+    MutantStack<int> assigned;
+    assigned = mstack;
+
+    std::cout << "Copy size: " << copy.size() << ", Assigned top: " << assigned.top() << std::endl;
+    
+    std::cout << "\n--- STL algorithms on MutantStack ---" << std::endl;
+    MutantStack<int> algostack;
+    for (int i = 1; i <= 5; ++i)
+        algostack.push(i * 10);  // 10, 20, 30, 40, 50
+    
+    std::cout << "Original: ";
+    printContainer(algostack);
+
+    std::cout << "\n--- std::find ---" << std::endl;
+    MutantStack<int>::iterator found = std::find(algostack.begin(), algostack.end(), 30);
+    if (found != algostack.end())
+        std::cout << "Found 30 at position: " << std::distance(algostack.begin(), found) << std::endl;
+
+    std::cout << "\n--- std::accumulate ---" << std::endl;
+    int sum = std::accumulate(algostack.begin(), algostack.end(), 0);
+    std::cout << "Sum of all elements: " << sum << std::endl;
+
+    std::cout << "\n--- std::reverse ---" << std::endl;
+    std::reverse(algostack.begin(), algostack.end());
+    std::cout << "After reverse: ";
+    printContainer(algostack);
+
+    std::cout << "\n--- std::sort (reverse back) ---" << std::endl;
+    std::sort(algostack.begin(), algostack.end());
+    std::cout << "After sort:    ";
+    printContainer(algostack);
+
+    std::cout << "\n--- std::min_element, std::max_element ---" << std::endl;
+    MutantStack<int>::iterator minIt = std::min_element(algostack.begin(), algostack.end());
+    MutantStack<int>::iterator maxIt = std::max_element(algostack.begin(), algostack.end());
+    if (minIt != algostack.end())
+        std::cout << "Min element: " << *minIt << std::endl;
+    if (maxIt != algostack.end()) {
+        std::cout << "Max element: " << *maxIt << std::endl;
+    }
+
+    std::cout << "\n--- easyfind on MutantStack ---" << std::endl;
+    MutantStack<int> efstack;
+    efstack.push(1);
+    efstack.push(2);
+    efstack.push(3);
+    efstack.push(4);
+    efstack.push(5);
+    printContainer(efstack);
+
+    std::cout << "Found: " << *easyfind(efstack, 3) << std::endl;
+    
 	return 0;
 }
